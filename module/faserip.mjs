b64529a9a13889433dc4b79df17aed2fa1238f2d
Bobby Nelson <bnelson52@gmail.comgit config --global user.email bnelson52@gmail.com>
2021-08-26 10:57:10 +0900

    removed Actor data as it was causing problems right now.

--- module/faserip.mjs
+++ module/faserip.mjs
@@ -1,7 +1,6 @@
 import { FASERIP } from "./helpers/config.mjs"
-import { FaseripActorSheet } from "./sheets/actor-sheet.mjs";
 import FaseripItemSheet from "./sheets/FaseripItemSheet.mjs";
-import FaseripActorSheet from "./sheets/actor-sheet.mjs"
+
 
 
 Hooks.once("init", function() {
@@ -12,6 +11,5 @@ Hooks.once("init", function() {
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("faserip", FaseripItemSheet, { makeDefault: true});
 
-    Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("faserip", FaseripActorSheet, { makeDefault: true});
+    
 })
